4827e564a72c4b063ff28ac2dbbe216586d5dd4c
Victor Niño <ninomtz.victor@gmail.com>
2026-06-06 15:14:14 -0600

    use good library for ulid

--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,1 @@
-const ENCODING = "0123456789ABCDEFGHJKMNPQRSTVWXYZ";
-const ENCODING_LEN = ENCODING.length;
-
-export function ulid(): string {
-  const now = Date.now();
-  const timeChars = Array.from<string>({ length: 10 });
-  let t = now;
-  for (let i = 9; i >= 0; i--) {
-    timeChars[i] = ENCODING[t % ENCODING_LEN] ?? "0";
-    t = Math.floor(t / ENCODING_LEN);
-  }
-  const randomPart = Array.from(
-    { length: 16 },
-    () => ENCODING[Math.floor(Math.random() * ENCODING_LEN)] ?? "0",
-  ).join("");
-  return timeChars.join("") + randomPart;
-}
+export { ulid } from "ulidx";
